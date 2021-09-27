8d92cdf66fe28d121ce31ebae76b867c59cc5a36
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2021-09-27 08:18:09 -0400

    week 2 work

---
+++ PerfectDay.java
@@ -0,0 +1,47 @@
+public class PerfectDay {
+  public static void main(String[] args) {
+    System.out.println("Here's my perfect day:");
+    morning(); //calling the function
+    afternoon();
+    night();
+
+  } //end main method
+
+// morning
+  public static void morning() {
+    System.out.print("In the morning, ");
+    System.out.println("I would try to wake up early so that I could have more time to complete tasks");
+    System.out.println("Afterwards I get ready and make breakfast.");
+    System.out.println("Then I finish up any tasks that I have left from school.");
+  } // end of morning
+
+// afternoon
+  public static void afternoon() {
+    System.out.print("In the afternoon, ");
+    System.out.println("I make some lunch to fill myself up.");
+    System.out.println("After finishing, I take a break and play some games with my friends.");
+  } //end of afternoon
+
+// night
+  public static void night() {
+    System.out.println("Once it reaches nighttime, I shower and eat dinner.");
+    System.out.print("After dinner, I would take out the trash");
+    System.out.println(" and chill for the rest of the night until bedtime.");
+  } //end of night
+
+
+} //end class
+
+
+
+// Draft
+/*In the morning I try to wake up early so that I could have more time to complete things
+Afterwards I get ready and make breakfast
+Then I finish up any tasks that I have left from school
+*/
+
+// In the afternoon, I make some lunch to fill myself up
+// After finishing, I take a break and play games with my friends
+
+//Once it reaches nighttime, I shower and eat dinner
+//After dinner, I would take out the trash and chill for the rest of the night until bedtime.
