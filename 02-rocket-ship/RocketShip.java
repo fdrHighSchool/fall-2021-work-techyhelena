be40f589e612dc4ced6834227b6f525ccbf34d7d
Helen Zhou <helenzhou@AC1MWK19GJC9H.local>
2021-09-28 08:47:04 -0400

    adjusted

---
+++ 02-rocket-ship/RocketShip.java
@@ -0,0 +1,21 @@
+public class RocketShip {
+  //make the triangle method
+  public static void triangle() {
+    System.out.println("   /\\"); // added an extra / because its a special charcater
+  } //closes triangle function
+
+  public static void square() {
+    System.out.println("+------+");
+    // System.out.println("+----");
+
+  } //closes square function
+
+  public static void main(String[] args) {
+    triangle();
+    square();
+
+} //closes main
+
+
+
+} // end class
--- RocketShip.java
+++
@@ -1,8 +0,0 @@
-public class RocketShip {
-  public static void main(String[] args) {
-  System.out.println("/\");
-  } // end main 
-
-
-
-} // end class 
