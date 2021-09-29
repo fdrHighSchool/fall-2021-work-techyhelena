53b2ad752f4fe3bc56c9556f9eda66896d5f0935
Helen Zhou <fdr,helenzhou@gmail.com>
2021-09-28 22:12:47 -0400

    git commit -m added more lines of code to make rocketship!

--- 02-rocket-ship/RocketShip.java
+++ 02-rocket-ship/RocketShip.java
@@ -1,24 +1,34 @@
 public class RocketShip {
   //make the triangle method
   public static void triangle() {
-    System.out.println("   /\\"); // added an extra / because its a special charcater
+    System.out.println("  /  \\");
+    System.out.println(" /    \\");
+    System.out.println("/      \\"); // added an extra / because its a special charcater
   } //closes triangle function
 
 
   //makes the vertical 
   public static void line() {
-    System.out.println("|     |");
-    System.out.println("|     |");
-  }
+    System.out.println("|      |");
+    System.out.println("|      |");
+  } //closes the vertical lines
 
+  //start of square function
   public static void square() {
     System.out.println("+------+");
     line();
+    System.out.println("+------+");
 
   } //closes square function
+
+  
+
+
+
   public static void main(String[] args) {
     triangle();
     square();
+
 
 
 } //closes main
