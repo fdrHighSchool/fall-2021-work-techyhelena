e15fb4fce5a5f758f0276da1586c83e821a89322
Helen Zhou <fdr,helenzhou@gmail.com>
2021-09-28 21:42:21 -0400

    git commit -m changes at night

--- 02-rocket-ship/RocketShip.java
+++ 02-rocket-ship/RocketShip.java
@@ -4,15 +4,22 @@ public class RocketShip {
     System.out.println("   /\\"); // added an extra / because its a special charcater
   } //closes triangle function
 
+
+  //makes the vertical 
+  public static void line() {
+    System.out.println("|     |");
+    System.out.println("|     |");
+  }
+
   public static void square() {
     System.out.println("+------+");
-    // System.out.println("+----");
+    line();
 
   } //closes square function
-
   public static void main(String[] args) {
     triangle();
     square();
+
 
 } //closes main
 
