502e955dad1f69da1e583f155f0451c1f4259ae9
Helen Zhou <fdr.helenzhou@gmail.com>
2021-10-17 15:26:04 -0400

    finished product!

--- 04- random-number-game/randomNum.java
+++ 04- random-number-game/randomNum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class randomNum {
+public class randomNum { 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in); //Create a Scanner Object
 
@@ -10,6 +10,21 @@ public class randomNum {
 
         // will give user feedback on whether or not they guessed it right
         int targetNum = ((int)(Math.random()*20 +1));
+        //output 
+        //if user guesses correctly 
+        if (userNum == targetNum) {
+            System.out.println("Good job, you've guessed it correctly!");
+        }
+        //if user guesses too high
+        if (userNum > targetNum) {
+            System.out.println("Too high, try again :)");
+        }
+        //if user guesses too low
+        if (userNum < targetNum) {
+            System.out.println("Too low, try again :)");
+        }
+        
 
+        myObj.close();
     } //closing bracket for main
 } //closing bracket for public class
