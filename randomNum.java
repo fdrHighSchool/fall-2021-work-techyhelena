94bc2c54b765f6450ec13a0053624fbaffaa1247
Helen Zhou <fdr.helenzhou@gmail.com>
2021-10-17 14:47:10 -0400

    updates on randomNum

--- randomNum.java
+++ randomNum.java
@@ -1,10 +1,15 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class randomNum {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in); //Create a Scanner Object
 
         //asks user to pick a number 
-        
+        System.out.println("Pick a number from 1-20");
+        int userNum = myObj.nextInt();
+
+        // will give user feedback on whether or not they guessed it right
+        int targetNum = ((int)(Math.random()*20 +1));
+
     } //closing bracket for main
 } //closing bracket for public class
