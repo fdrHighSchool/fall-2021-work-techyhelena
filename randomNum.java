07bbd07f7fec3ad36612d71dace8ea0f06987067
Helen Zhou <fdr.helenzhou@gmail.com>
2021-10-17 14:25:37 -0400

    added random number game

---
+++ randomNum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class randomNum {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in); //Create a Scanner Object
+
+        //asks user to pick a number 
+        
+    } //closing bracket for main
+} //closing bracket for public class
