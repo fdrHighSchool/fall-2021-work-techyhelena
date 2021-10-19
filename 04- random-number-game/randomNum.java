1916b0903340aafe352a3e23aecc757a628dadd5
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2021-10-19 08:47:01 -0400

    work on debugging scanner function and if statement

--- 04- random-number-game/randomNum.java
+++ 04- random-number-game/randomNum.java
@@ -4,13 +4,16 @@ import java.util.Response;
 public class randomNum {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in); //Create a Scanner Object
-        Scanner userResponse = new Scanner(System.in); //another scanner object
         //welcomes user into the game
+        int userResponse = myObj.nextLine();
         System.out.println("Welcome, would you like to play a game?" + "\nYes (y)" + "No (n)");
-        
+        if(userResponse.equals("y")){
+          //asks user to pick a number
+          System.out.println("Pick a number from 1-20");
+        } else {
+          System.out.println("Aw man.. maybe you'll change your mind later? :D");
+        } //closes else
 
-        //asks user to pick a number
-        System.out.println("Pick a number from 1-20");
         //variable for the number of tries user has
         int userTries = 3;
         int userNum = myObj.nextInt();
@@ -21,29 +24,29 @@ public class randomNum {
         //if user guesses correctly
         if (userNum == targetNum) {
             System.out.println("Good job, you've guessed it correctly!");
-        } else {
-          //loops game whenever user doesn't get the correct answer
-          for (int i=0; i < 2; i++) {
-            //if user guesses too high
-            if (userNum > targetNum) {
-                System.out.println("Your number was too high :^0" + "You have " + userNum - 1  " left!");
-
-            }
-
-          }
         }
+
+          //loops game whenever user doesn't get the correct answer
+          // for (int i=0; i < 2; i++) {
+            //if user guesses too high
+        //     if (userNum > targetNum) {
+        //         System.out.println("Your number was too high :^0" + "You have " + userTries " left!");
+        //
+        //     }
+        //
+        //   }
+        // }
 
         //if user guesses too high
         // if (userNum > targetNum) {
         //     System.out.println("Your number was too high :^0" + "\nThe number was " + targetNum + "\nThank you for playing!");
-        }
+        // }
         //if user guesses too low
         // if (userNum < targetNum) {
         //     System.out.println("Your number was too low :'(" + "\nThe number was " + targetNum + "\nThank you for playing!");
         // }
 
 
-
-        myObj.close();
+        // myObj.close();
     } //closing bracket for main
 } //closing bracket for public class
