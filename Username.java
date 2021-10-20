8d06bde07fb51197a6747b15d5df5ef6fd9011dc
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2021-10-20 08:42:38 -0400

    added substring functions for first and last name

--- Username.java
+++ Username.java
@@ -13,11 +13,17 @@ public class Username {
 
 
       //ask the user their birthday in the form mm/dd/yy
-      System.out.print("Hi, " + fName + ", please enter your birthday in mm/dd/yy format");
+      System.out.print("Hi, " + fName + ", please enter your birthday in mm/dd/yy format: ");
       String birthday = s.nextLine();
 
       //generate a username for that person based on their data
-      System.out.println(fName + " " + lName + " " + birthday);
+      String username = "";
+      //takes the first letter of the first name 
+      username += fName.substring(0, 1);
+      //takes the final two letters of the last name (depending on how long they are)
+      username += lName.substring(length() - 2);
 
+      System.out.print("Here's your username, please keep it in a safe space.. or else..: ");
+      System.out.println(username);
   } //end main method
 } //end class
