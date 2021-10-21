f7966b9834ab9d40d056648fccb0fa75cecf0631
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2021-10-21 08:39:32 -0400

    finished product

--- Username.java
+++ Username.java
@@ -2,28 +2,53 @@ import java.util.*;
 
 public class Username {
   public static void main(String[] args) {
-      //create an instance of the scanner
+      // create an instance of the Scanner
       Scanner s = new Scanner(System.in);
-      //ask the user their first name
+
+      // ask the user their first name
       System.out.print("Please enter your first name: ");
       String fName = s.nextLine();
-      //ask the user their last name
+
+      // ask the user their last name
       System.out.print("Please enter your last name: ");
       String lName = s.nextLine();
 
-
-      //ask the user their birthday in the form mm/dd/yy
-      System.out.print("Hi, " + fName + ", please enter your birthday in mm/dd/yy format: ");
+      // ask the user their birthday in the form mm/dd/year
+      System.out.print("Hi, " + fName + ", please enter your birthday in mm/dd/year format: " );
       String birthday = s.nextLine();
 
-      //generate a username for that person based on their data
+      // generate a username for that person based on their data
       String username = "";
-      //takes the first letter of the first name 
-      username += fName.substring(0, 1);
-      //takes the final two letters of the last name (depending on how long they are)
-      username += lName.substring(length() - 2);
 
-      System.out.print("Here's your username, please keep it in a safe space.. or else..: ");
+      // only the first letter of the first name
+      username += fName.substring(0, 1);
+
+      // final two letters of the last name (depends on how long the name is)
+      username += lName.substring(lName.length() - 2);
+
+      // grab the month of birth (must be 2 characters)
+      if (birthday.indexOf("/") == 2) {
+        username += birthday.substring(0, 2);
+      } else {
+        username += "0";
+        username += birthday.substring(0, 1);
+      } // end if/else
+
+      // grab last two digits of birthyear
+      username += birthday.substring(birthday.length() - 2);
+
+      // add domain name
+      username += "@anonymous.org";
+
+      System.out.println("Here's your username, and keep it safe... or else..");
       System.out.println(username);
-  } //end main method
-} //end class
+
+      System.out.println("─────█─▄▀█──█▀▄─█─────");
+      System.out.println("────▐▌──────────▐▌────");
+      System.out.println("────█▌▀▄──▄▄──▄▀▐█────");
+      System.out.println("───▐██──▀▀──▀▀──██▌───");
+      System.out.println("──▄████▄──▐▌──▄████▄──");
+
+  } // end main method
+
+} // end class
