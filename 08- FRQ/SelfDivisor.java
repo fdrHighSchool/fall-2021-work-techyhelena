9cf737c34f7ebbe10d2965eed356c784d272ec59
Helen Zhou <58716145+techyhelena@users.noreply.github.com>
2022-01-05 22:46:13 -0500

    laid out plan for part A

---
+++ 08- FRQ/SelfDivisor.java
@@ -0,0 +1,26 @@
+import java.util*;
+
+public class SelfDivisor {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  } // closes main 
+
+  /* @param number: the number to be tested 
+      Precondition: number > 0
+     @return true if every decimal digit of number is a divisior or number; false otherwise
+  */
+  public static boolean isSelfDivisor(int number) {
+    // loop to go through every value in number
+    for(int i = 0; number > 0; i++) {
+      // checking if number is self divisable 
+      // zero counts as an automatic elimination (false statement)
+      if(number  ){
+
+      } // end if statement 
+
+    } // closes for loop 
+
+    return 0;
+  } // closes method 
+
+} // closes class 
