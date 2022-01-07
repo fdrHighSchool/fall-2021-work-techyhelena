87924c14f874e4c040ddc996e1f0206507268db1
Helen Zhou <58716145+techyhelena@users.noreply.github.com>
2022-01-06 22:37:43 -0500

    finished part a

--- 08- FRQ/SelfDivisor.java
+++ 08- FRQ/SelfDivisor.java
@@ -1,32 +1,40 @@
-import java.util*;
+import java.util.*;
 
 public class SelfDivisor {
   public static void main(String[] args) {
-    int testCase = 15;
+    //User Testing 
+    Scanner s = new Scanner(System.in);
+    System.out.println("Self Divisor Test");
+    int userNum = s.nextInt();
+
+    System.out.println(userNum + " is a self divisior: " + isSelfDivisor(userNum));
+
+    // Manual Testing
+    System.out.println("15 is a self divisor: " +isSelfDivisor(15)); // true 
+    System.out.println("256 is a self divisor: " + isSelfDivisor(256)); // false 
+    System.out.println("20 is a self divisor: " + isSelfDivisor(20)); // false
   } // closes main
 
   /* @param number: the number to be tested
       Precondition: number > 0
      @return true if every decimal digit of number is a divisior or number; false otherwise
   */
-  public static boolean isSelfDivisor(int number) {
-    // loop to go through every value in number
-    // chop the number down by 10
-    while(number >  0) {
-      // checking if each value in number is self divisable
-      // use mod -> if the mod of a number is not 0 then there's a remainder (not self dvisiable)
-      int answer = testCase;
-      System.out.println(testCase % 10);
-      System.out.printn(testCase / 10);
-      // zero counts as an automatic elimination (false statement)
-      if(){
+  public static boolean isSelfDivisor(int number) {    
+    int digit = 0;
+    while(number > 0) {      
+      // use mod to extract digits 
+      digit = number % 10; 
 
-      } // end if statement
+      // zero counts as an automatic elimination 
+      // mod != 0 means there is a remainder
+      if(digit == 0 || number % digit != 0) {
+        return false;
+      } // close if statement     
 
-
+      // division to reduce digits     
+      number = number / 10; 
     } // closes while loop
-
-    return 0;
+    return true;
   } // closes method
 
 } // closes class
