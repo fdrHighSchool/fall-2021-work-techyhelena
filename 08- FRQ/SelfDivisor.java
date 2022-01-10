7be04e1ab7582df292308d8eaf88196eb9a53ef1
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2022-01-10 08:48:10 -0500

    finalize

--- 08- FRQ/SelfDivisor.java
+++ 08- FRQ/SelfDivisor.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class SelfDivisor {
   public static void main(String[] args) {
-    //User Testing 
+    //User Testing
     Scanner s = new Scanner(System.in);
     System.out.println("Self Divisor Test");
     int userNum = s.nextInt();
@@ -10,8 +10,8 @@ public class SelfDivisor {
     System.out.println(userNum + " is a self divisior: " + isSelfDivisor(userNum));
 
     // Manual Testing
-    System.out.println("15 is a self divisor: " +isSelfDivisor(15)); // true 
-    System.out.println("256 is a self divisor: " + isSelfDivisor(256)); // false 
+    System.out.println("15 is a self divisor: " +isSelfDivisor(15)); // true
+    System.out.println("256 is a self divisor: " + isSelfDivisor(256)); // false
     System.out.println("20 is a self divisor: " + isSelfDivisor(20)); // false
   } // closes main
 
@@ -19,22 +19,39 @@ public class SelfDivisor {
       Precondition: number > 0
      @return true if every decimal digit of number is a divisior or number; false otherwise
   */
-  public static boolean isSelfDivisor(int number) {    
+  public static boolean isSelfDivisor(int number) {
     int digit = 0;
-    while(number > 0) {      
-      // use mod to extract digits 
-      digit = number % 10; 
+    while(number > 0) {
+      // use mod to extract digits
+      digit = number % 10;
 
-      // zero counts as an automatic elimination 
+      // zero counts as an automatic elimination
       // mod != 0 means there is a remainder
       if(digit == 0 || number % digit != 0) {
         return false;
-      } // close if statement     
-
-      // division to reduce digits     
-      number = number / 10; 
+      } // close if statement
+      // division to reduce digits
+      number = number / 10;
     } // closes while loop
     return true;
+  } // closes method
+
+  public static int[] firstNumSelfDivisors (int, start, int num) {
+  // start -> starting point for values to be checked
+  // num -> the size of the array to be returned
+
+  // initialize array size by the num
+  int[] collection = new int[num];
+  // while the last value in the array is not zero, keep searching (while loop)
+  int count = 0; // making a counter to check if array is full
+  while(count > num) {
+
+  }
+  // to check if the numbers should be in the array -> call isSelfDivisor
+  // add value if true
+
+  // return the array once filled
+
   } // closes method
 
 } // closes class
