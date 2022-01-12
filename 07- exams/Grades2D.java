749a676f4034285a29c65dbfb8448014ddfbfd76
Helen Zhou <helenzhou@ac1mwk19gjc9h.78b505.nycboe.org>
2022-01-12 08:47:51 -0500

    updates

--- 07- exams/Grades2D.java
+++ 07- exams/Grades2D.java
@@ -1,14 +1,27 @@
 //REFERENCE TO 2D ARRAY: https://www.geeksforgeeks.org/multidimensional-arrays-in-java/
+import java.util.   *;
 public class Grades2D {
   public static void main(String[] args) {
     // 15 stydents, each with 5 grades
     // initialize
     // [row index] [column index]
     int[][] studentGrades = new int[15][5];
-    // fill & access
-    for(int i = 0; i < studentGrades.length; i++) {
-      studentGrades[i] = (int)(Math.random()* 46) +55;
-    }
+    // accessing
+    System.out.println(studentGrades[studentGrades.length-1][studentGrades[0].length - 1]);
+    System.out.println(studentGrades.length); // = 15
+    System.out.println(studentGrades[0].length); // = 5
+
+    int num = 1;
+    // going thru every row
+    for (int row = 0; row < studentGrades.length; row++) {
+      // going thru every col
+      for(for int col = 0; col < studentsGrades[0].length; col++) {
+        //filling numbers in each row and col
+        studentGrades[row][col] = num;
+        num++;
+
+      } // end col loop
+    } // end row loop
     // print
   }
 
